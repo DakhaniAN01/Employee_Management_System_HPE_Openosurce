7a4bf4eb4f9897e91f3b76ad89026f6ba2e64175
DakhaniAN01 <dakhani.an01@gmail.com>
2024-09-10 20:17:48 +0530

    Test Case Added

--- src/main/java/com/hpe/EmployeeManager.java
+++ src/main/java/com/hpe/EmployeeManager.java
@@ -8,10 +8,10 @@ public class EmployeeManager {
     }
 
     private void initializeEmployees() {
-        /*employees.getEmployeeList().add(new Employee(101, "Gautam", "Patil", "patil.g01@gmail.com", "Software Engineer"));
+        employees.getEmployeeList().add(new Employee(101, "Gautam", "Patil", "patil.g01@gmail.com", "Software Engineer"));
         employees.getEmployeeList().add(new Employee(102, "Alisha", "Smith", "alisha.smith@hotmail.com", "Project Manager"));
         employees.getEmployeeList().add(new Employee(103, "Kim", "Son", "sonkim99@yahoo.com", "HR Specialist"));
-        employees.getEmployeeList().add(new Employee(104, "Zahira", "Sheikh", "shieh.zahira@outlook.com", "Marketing Manager"));*/
+        employees.getEmployeeList().add(new Employee(104, "Zahira", "Sheikh", "shieh.zahira@outlook.com", "Marketing Manager"));
     }
 
     public Employees getEmployees() {
---
+++ src/test/java/com/hpe/EmployeeControllerTest.java
@@ -0,0 +1,53 @@
+package com.hpe;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class EmployeeControllerTest {
+     @Mock
+    private EmployeeManager employeeManager;
+
+    @InjectMocks
+    private EmployeeController employeeController;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetAllEmployees() {
+        Employees employees = new Employees();
+        employees.getEmployeeList().add(new Employee(101, "Gautam", "Patil", "patil.g01@gmail.com", "Software Engineer"));
+
+        when(employeeManager.getEmployees()).thenReturn(employees);
+
+        Employees result = employeeController.getAllEmployees();
+
+        assertEquals(1, result.getEmployeeList().size());
+        assertEquals("Gautam", result.getEmployeeList().get(0).getFirstName());
+    }
+
+    @Test
+    public void testAddEmployee() {
+        Employee newEmployee = new Employee(105, "Aditya", "Shinde", "aditya.shinde@gmail.com", "DevOps Engineer");
+        Employees employees = new Employees();
+
+        when(employeeManager.getEmployees()).thenReturn(employees);
+
+        ResponseEntity<String> response = employeeController.addEmployee(newEmployee);
+
+        assertEquals("Employee added successfully", response.getBody());
+        assertEquals(1, employees.getEmployeeList().size());
+        assertEquals("Aditya", employees.getEmployeeList().get(0).getFirstName());
+    }
+
+    
+}
