ecc8c15a91473f7f4d7f970e90c21fe51c205142
DakhaniAN01 <dakhani.an01@gmail.com>
2024-09-10 18:50:12 +0530

    Employee Model added

---
+++ src/main/java/com/hpe/Employee.java
@@ -0,0 +1,39 @@
+package com.hpe;
+
+public class Employee {
+    private Long employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String title;
+
+    
+    public Employee(Long employeeId, String firstName, String lastName, String email, String title) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.title = title;
+    }
+
+    
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
